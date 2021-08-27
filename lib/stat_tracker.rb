df2e375bd4f8f5c0dc5eb7abde99f228e95d0fd5
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-27 14:51:12 -0500

    Add game class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,7 @@
 class StatTracker
   def self.from_csv(locations)
-
+    @game_manager = GameManager.new(locations[:games])
+    @team_manager = TeamManager.new(locations[:teams])
+    @game_team_manager = GameTeamManager.new(locations[:game_teams])
   end
 end
