8403598f19d15d1fbb803d2a87d1c6aa9947fa3d
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-27 17:03:00 -0500

    Convert 'string-integers' to Integers

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -17,20 +17,20 @@ class GameTeam
               :giveaways,
               :takeaways
   def initialize(data)
-    @game_id = data[:game_id],
-    @team_id = data[:team_id],
+    @game_id = data[:game_id].to_i,
+    @team_id = data[:team_id].to_i,
     @hoa = data[:hoa],
     @result = data[:result],
     @settled_in = data[:settled_in],
     @head_coach = data[:head_coach],
-    @goals = data[:goals],
-    @shots = data[:shots],
-    @tackles = data[:tackles],
-    @pim = data[:pim],
-    @power_play_opps = data[:powerplayopportunities],
-    @face_off_win_percentage = data[:faceoffwinpercentage],
-    @giveaways = data[:giveaways],
-    @takeaways = data[:takeaways]
+    @goals = data[:goals].to_i,
+    @shots = data[:shots].to_i,
+    @tackles = data[:tackles].to_i,
+    @pim = data[:pim].to_i,
+    @power_play_opps = data[:powerplayopportunities].to_i,
+    @face_off_win_percentage = data[:faceoffwinpercentage].to_f,
+    @giveaways = data[:giveaways].to_i,
+    @takeaways = data[:takeaways].to_i
   end
 
   def self.all
