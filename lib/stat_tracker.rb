8437e0b214bdc3117ae4f3a9081c5130df029238
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-09-02 09:56:06 -0500

    Remove unused code

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,5 @@
 class StatTracker
   def self.from_csv(locations)
-    @game_manager = GameManager.new(locations[:games])
-    @team_manager = TeamManager.new(locations[:teams])
-    @game_team_manager = GameTeamManager.new(locations[:game_teams])
+    
   end
 end
