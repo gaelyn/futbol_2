9a8b42d8958726e35a0412c1ef015ada7b6772e4
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-09-08 16:19:26 -0500

    Add lowest total score to Game Stats

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -5,4 +5,11 @@ class GameStats
     end
     high_scoring_game.away_goals + high_scoring_game.home_goals
   end
+
+  def self.lowest_total_score
+    low_scoring_game = Game.all.min_by do |game|
+      game.away_goals + game.home_goals
+    end
+    low_scoring_game.away_goals + low_scoring_game.home_goals
+  end
 end
--- test/game_stats_test.rb
+++ test/game_stats_test.rb
@@ -4,4 +4,8 @@ class GameStatsTest < Minitest::Test
   def test_highest_total_score_returns_highest_sum_of_both_teams_scores
     assert_equal 11, GameStats.highest_total_score
   end
+
+  def test_lowest_total_score_returns_lowest_sum_of_both_teams_scores
+    assert_equal 0, GameStats.lowest_total_score
+  end
 end
