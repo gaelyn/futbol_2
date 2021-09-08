174cf92858fb21a977e2e55c317129061ca6e0dd
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-09-08 15:58:05 -0500

    Refactor test helper

---
+++ lib/helper.rb
@@ -0,0 +1,8 @@
+require 'csv'
+
+require_relative './modules/finder'
+require_relative './modules/data_loader'
+
+require_relative './game'
+require_relative './team'
+require_relative './game_team'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/modules/finder'
-require './lib/game'
-require './lib/team'
-require './lib/game_team'
+
+require_relative '../lib/helper'
+# require './lib/modules/finder'
+# require './lib/game'
+# require './lib/team'
+# require './lib/game_team'
