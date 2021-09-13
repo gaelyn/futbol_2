3c5ef26d9fd18fb2037606ca9b0b401ed4272d4f
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-09-13 12:25:39 -0500

    Get rid of comments

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -7,16 +7,4 @@ class GameStats
     end
     high_scoring_game.away_goals + high_scoring_game.home_goals
   end
-
-  # def self.lowest_total_score
-  #   low_scoring_game = Game.all.min_by do |game|
-  #     game.away_goals + game.home_goals
-  #   end
-  #   low_scoring_game.away_goals + low_scoring_game.home_goals
-  # end
-  #
-  # def self.percent_home_wins
-  #   home_wins = GameTeam.where(hoa: "home", result: "WIN")
-  #   home_wins / Game.all.count
-  # end
 end
--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -9,12 +9,4 @@ class StatTracker
   def highest_total_score
     GameStats.highest_total_score
   end
-
-  # def lowest_total_score
-  #   GameStats.lowest_total_score
-  # end
-  #
-  # def percent_home_wins
-  #   GameStats.percent_home_wins
-  # end
 end
